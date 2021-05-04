088cf9306329eb45e55eb863e85d81092f704923
Sakib Mohammed <sakibmd.cse@gmail.com>
2021-05-04 16:32:02 +0600

    add a clock

--- js/script.js
+++ js/script.js
@@ -22,3 +22,34 @@ const Covid19 = {
     }
 }
 Vue.createApp(Covid19).mount("#covid-update");
+
+
+function currentTime() {
+    var date = new Date(); /* creating object of Date class */
+    console.log(date)
+    var midday = "AM";
+    var day = date.getDate();
+    var month = date.getMonth();
+    var year = date.getFullYear();
+
+    var hour = date.getHours();
+    var min = date.getMinutes();
+    var sec = date.getSeconds();
+    hour = updateTime(hour);
+    midday = (hour >= 12) ? "PM" : "AM";
+    min = updateTime(min);
+    sec = updateTime(sec);
+    document.getElementById("clock").innerText = day + "/" + month + "/" + year + " - " + hour + ":" + min + ":" + sec + ' ' +
+        midday; /* adding time to the div */
+    var t = setTimeout(function() { currentTime() }, 1000); /* setting timer */
+}
+
+function updateTime(k) {
+    if (k < 10) {
+        return "0" + k;
+    } else {
+        return k;
+    }
+}
+
+currentTime(); /* calling currentTime() function to initiate the process */
